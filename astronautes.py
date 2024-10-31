4f2b675959cb654d4d97f61058f75914ead9b022
MBOUMBA MOUSSAVOU Junior Melcki <juniormboumba98@gmail.com>
2024-10-31 22:51:27 +0100

    Ajout du nom en commentaires

--- astronautes.py
+++ astronautes.py
@@ -4,3 +4,6 @@ contenu = requests.get("http://api.open-notify.org/astros.json")
 for personne in contenu.json()["people"]:
     if personne['craft'] == "ISS": 
         print(personne)
+
+
+#MBOUMBA MOUSSAVOU Junior Melcki
